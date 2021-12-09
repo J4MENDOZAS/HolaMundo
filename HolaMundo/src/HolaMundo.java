22e089b0b167a03159174569d8c038f7b4f66b95
Joseph Mendoza <jmendozas@quality-services.mx>
2021-12-08 23:28:52 -0600

    Nuevo Mensaje

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo - NFT");
 		System.out.println("Etapa 1.0");
-
+		System.out.println("Prueba 1");
 	}
 
 }
