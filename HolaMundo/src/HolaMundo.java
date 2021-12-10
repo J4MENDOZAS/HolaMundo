796ced2b03d7f5460a3afd8b0a1a4629db824230
Joseph Mendoza <jmendozas@quality-services.mx>
2021-12-10 13:58:48 -0600

    Se modifica Mensaje etapa

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,18 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo - NFT");
 		System.out.println("Etapa 1.0");
+		System.out.println("Etapa 2.0");
+		System.out.println("Etapa 3.0");
+		System.out.println("Etapa 4.0");
+		System.out.println("Etapa 5.0");
+		System.out.println("Etapa 6.0");
+		System.out.println("Etapa 7.0");
+		System.out.println("Etapa 8.0");
+		System.out.println("Etapa 9.0");
+		System.out.println("Etapa 10.0");
+		System.out.println("Etapa 11.0");
+		System.out.println("Etapa 12.0");
+		System.out.println("Etapa 13.0");
 		}
 
 }
